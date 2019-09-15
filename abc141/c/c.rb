9f6b4fc2d352353cea32ced23b3c3e384add551b
sooo <mail@sooo.space>
2019-09-15 22:44:59 +0900

    fix abc141 c

--- abc141/c/c.rb
+++ abc141/c/c.rb
@@ -2,7 +2,7 @@ player_num,first_point,q = gets.chomp.split.map(&:to_i)
 a = []
 q.times{ a << gets.to_i }
 
-players_points = Array.new(player_num, first_point)
+players_points = Array.new(player_num, first_point - a.length)
 
 def p_winner arr
   arr.each do |point|
@@ -11,10 +11,8 @@ def p_winner arr
 end
 
 def move_round players_points, a
-  a.each do |ansered_player|
-    players_points.map!.with_index(1) do |point, i|
-      next i == ansered_player ? point : point - 1
-    end
+  a.each do |ansered_player_index|
+    players_points[ansered_player_index - 1] = players_points[ansered_player_index - 1] + 1
   end
   return players_points
 end
