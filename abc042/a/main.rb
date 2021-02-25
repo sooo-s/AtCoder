ca118473c20367a1897c0dffc396ee2be1183ed4
sooo <mail@sooo.space>
2021-02-25 19:18:12 +0900

    init abc042

---
+++ abc042/a/main.rb
@@ -0,0 +1,6 @@
+n = gets.to_i # 10
+s = gets.chomp.to_s #  "string"
+s = gets.chomp.chars # ["s", "t", "r", "i", "n", "g"]
+n,m = gets.split.map(&:to_i) # n = 10, m = 20
+q.times{ array << gets.to_i } # [1, 2, 3]
+
