cb8328bcf9632f1184088b10cfc755928a93fe84
sooo-s <mail@sooo.space>
2021-03-02 22:49:56 +0900

    abc185 AC b

--- abc185/b/main.rb
+++ abc185/b/main.rb
@@ -2,4 +2,19 @@ n, m, t = gets.chomp.split.map(&:to_i)
 ab = []
 m.times { ab.push gets.chop.split.map(&:to_i) }
 
-puts n, m, t, ab
+n_max = n
+walk = true
+[0, ab, t].flatten.each_cons(2) do |a, b|
+  if walk
+    n -= b - a
+    if n <= 0
+      puts 'No'
+      exit
+    end
+  else
+    n = [n + b - a, n_max].min
+  end
+  walk = !walk
+end
+
+puts 'Yes'
