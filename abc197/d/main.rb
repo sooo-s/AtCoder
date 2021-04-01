4e4e977b3c37d81b6557e8fc8d7d06f9ab991f7f
sooo-s <mail@sooo.space>
2021-04-02 02:41:57 +0900

    abc197 AC d

--- abc197/d/main.rb
+++ abc197/d/main.rb
@@ -1,6 +1,10 @@
 n = gets.to_i # 10
-s = gets.chomp.to_s #  "string"
-s = gets.chomp.chars # ["s", "t", "r", "i", "n", "g"]
-n,m = gets.split.map(&:to_i) # n = 10, m = 20
-n.times{ array << gets.split.map(&:to_i) } # [[1, 2], [3, 4], [5, 6]]
+x0, y0 = gets.split.map(&:to_i) # n = 10, m = 20
+xn2, yn2 = gets.split.map(&:to_i) # n = 10, m = 20
 
+p0 = Complex(x0, y0)
+pn2 = Complex(xn2, yn2)
+center = (p0 + pn2) / 2
+p1 = (p0 - center) * Complex.polar(1, 2 * Math::PI / n) + center
+
+puts p1.rect.join(' ')
