31adc67eaa07499f32e7077ecbe9767e2c403d1f
sooo <mail@sooo.space>
2021-06-06 22:24:28 +0900

    update template

--- bin/ruby-template/main.rb
+++ bin/ruby-template/main.rb
@@ -1,6 +1,5 @@
 n = gets.to_i # 10
 s = gets.chomp.to_s #  "string"
 s = gets.chomp.chars # ["s", "t", "r", "i", "n", "g"]
-n,m = gets.split.map(&:to_i) # n = 10, m = 20
-n.times{ array << gets.split.map(&:to_i) } # [[1, 2], [3, 4], [5, 6]]
-
+n, m = gets.chomp.split.map(&:to_i) # n = 10, m = 20
+n.times { array << gets.split.map(&:to_i) } # [[1, 2], [3, 4], [5, 6]]
