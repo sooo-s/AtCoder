df046506ede697f25617232733da4777b316ab23
sooo-s <mail@sooo.space>
2021-07-25 01:16:14 +0900

    abc204 AC c

--- abc204/c/main.rb
+++ abc204/c/main.rb
@@ -1,5 +1,27 @@
-n, m = gets.chomp.split.map(&:to_i) # n = 10, m = 20
-ab = []
-n.times { ab << gets.split.map(&:to_i) } # [[1, 2], [3, 4], [5, 6]]
+N, M = gets.chomp.split.map(&:to_i) # n = 10, m = 20
+AB = M.times.map { gets.split.map(&:to_i) } # [[1, 2], [3, 4], [5, 6]]
 
-p n, m, ab
+g = Array.new(N + 1) { [] }
+AB.each do |a, b|
+  g[a] << b
+end
+
+ans = 0
+1.upto(N) do |start|
+  visited = Array.new(N + 1)
+  queue = [start]
+  visited[start] = true
+
+  until queue.empty?
+    v = queue.shift
+    g[v].each do |to|
+      unless visited[to]
+        queue.push to
+        visited[to] = true
+      end
+    end
+  end
+  ans += visited.count(true)
+end
+
+puts ans
